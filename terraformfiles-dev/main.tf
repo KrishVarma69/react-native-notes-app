3f950e0537da8db79e58c14bccc81ce2e75642fd
KrishVarma69 <52405615+KrishVarma69@users.noreply.github.com>
2025-01-28 03:50:14 +0530

    Update main.tf

--- terraformfiles-dev/main.tf
+++ terraformfiles-dev/main.tf
@@ -1,17 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1a" # Change to your preferred region
+  region = var.region
 }
 
-variable "ec2_key_name" {
-  description = "The name of the EC2 Key Pair"
-  type        = string
+# Define variables
+variable "region" {
+  default = "us-west-2"
 }
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ami" {
+  default = "ami-0c55b159cbfafe1f0"  # Replace with a valid Ubuntu AMI ID for your region
+}
+variable "key_name" {}
+variable "private_key_path" {}
 
-# Create a Security Group for EC2
-resource "aws_security_group" "app_sg" {
-  name_prefix = "react_notes_app_sg"
+# Security group
+resource "aws_security_group" "allow_ssh_http" {
+  vpc_id = aws_vpc.main.id
 
-  # Allow SSH Access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -19,7 +35,6 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow HTTP Traffic
   ingress {
     from_port   = 80
     to_port     = 80
@@ -27,7 +42,13 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -36,30 +57,76 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-# Provision EC2 Instance
-resource "aws_instance" "react_notes_app" {
-  ami           = "ami-04b4f1a9cf54c11d0" # Amazon ubuntu AMI
-  instance_type = "t2.micro"
-  key_name      = var.ec2_key_name
-  security_groups = [
-    aws_security_group.app_sg.name
-  ]
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "react-native-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "react-native-subnet"
+  }
+}
+
+# Network interface
+resource "aws_network_interface" "main" {
+  subnet_id = aws_subnet.main.id
+
+  tags = {
+    Name = "react-native-nic"
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "app" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  network_interface {
+    network_interface_id = aws_network_interface.main.id
+    device_index         = 0
+  }
+  security_groups        = [aws_security_group.allow_ssh_http.name]
+  associate_public_ip_address = true
 
   tags = {
     Name = "ReactNativeNotesApp"
   }
 
-  # User Data to Bootstrap the Instance
   user_data = <<-EOF
     #!/bin/bash
-    sudo yum update -y
-    curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-    sudo yum install -y nodejs git
-    npm install pm2@latest -g
+    apt-get update
+    apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get update
+    apt-get install -y docker-ce
+    systemctl start docker
+    systemctl enable docker
+    usermod -aG docker ubuntu
   EOF
 }
 
-# Output Public IP of EC2 Instance
-output "ec2_public_ip" {
-  value = aws_instance.react_notes_app.public_ip
+output "instance_ip" {
+  value = aws_instance.app.public_ip
 }
+
+
+# Below are the commands to execute the terraform file
+    # terraform init
+    # terraform plan -var-file="terraform.tfvars"
+    # terraform apply -var-file="terraform.tfvars"
